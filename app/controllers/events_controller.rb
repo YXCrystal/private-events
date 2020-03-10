6f535e3633bd4c39194fea7532dd815cc5a278a9
Crystal <yxcwuu@gmail.com>
2020-03-10 16:32:35 -0500

    Add create action and redirect to show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,24 @@ class EventsController < ApplicationController
     def new
         @event = Event.new
     end
+
+    def create
+        @event = Event.create(event_params)
+        if @event.save
+            redirect_to @event
+        else
+            flash.now[:notice] = "Failed to create event"
+            render 'new'
+        end
+    end
+
+    def show
+        @event = Event.find(params[:id])
+    end
+
+    private
+    
+    def event_params
+        params.require(:event).permit(:title, :description, :location, :time)
+    end
 end
