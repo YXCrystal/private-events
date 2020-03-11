778271d56e0a82347a798a7c87de9a8ef1215123
Crystal <yxcwuu@gmail.com>
2020-03-11 11:59:04 -0500

    Add navigation links and styling

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
     
     def index
-        @events = Event.all 
+        @events = Event.order(time: :desc) 
     end
     def new
         @event = Event.new
